6937357aa44d21dff35a565af479adc992273395
potatomasterrace <reda.bourial@gmail.com>
2021-01-03 22:46:01 +0100

    implemented misc

--- mpz_misc.go
+++ mpz_misc.go
@@ -1,11 +1,41 @@
 package main
 
+// #cgo LDFLAGS: -lgmp
+// #include "mpz.h"
+import "C"
+
 // Miscellaneous Functions
 // int pmpz_even_p(const unsafe_mpz op);
+func (op Mpz) MpzEvenP() C.int {
+	return C.pmpz_even_p(op.Ptr())
+}
+
 // int pmpz_fits_sint_p(const unsafe_mpz op);
+func (op Mpz) MpzFitsSintP() C.int {
+	return C.pmpz_even_p(op.Ptr())
+}
+
 // int pmpz_fits_slong_p(const unsafe_mpz op);
+func (op Mpz) MpzFitsSlongP() C.int {
+	return C.pmpz_fits_slong_p(op.Ptr())
+}
+
 // int pmpz_fits_sshort_p(const unsafe_mpz op);
+func (op Mpz) MpzFitsSshortP() C.int {
+	return C.pmpz_fits_sshort_p(op.Ptr())
+}
+
 // int pmpz_fits_uint_p(const unsafe_mpz op);
+func (op Mpz) MpzFitsUintP() C.int {
+	return C.pmpz_fits_uint_p(op.Ptr())
+}
+
 // int pmpz_fits_ulong_p(const unsafe_mpz op);
+func (op Mpz) MpzFitsUlongP() C.int {
+	return C.pmpz_fits_ulong_p(op.Ptr())
+}
+
 // int pmpz_fits_ushort_p(const unsafe_mpz op);
-// size_t pmpz_sizeinbase(const unsafe_mpz op, int base);
+func (op Mpz) MpzFitsUshortP() C.int {
+	return C.pmpz_fits_ushort_p(op.Ptr())
+}
