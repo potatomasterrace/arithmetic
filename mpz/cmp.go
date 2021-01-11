ce4458b71b659d4d8b12c04b431d463cc7c43522
potatomasterrace <reda.bourial@gmail.com>
2021-01-11 09:36:22 +0100

    refractored types in cmp

--- mpz/cmp.go
+++ mpz/cmp.go
@@ -5,41 +5,49 @@ import "C"
 
 // Comparison Functions
 // int pmpz_cmp(const unsafe_mpz op1, const unsafe_mpz op2);
-func (m Mpz) CmpMpz(mpz Mpz) C.int {
-	return C.pmpz_cmp(m.Ptr(), mpz.Ptr())
+func (m Mpz) Cmp(mpz Mpz) int {
+	ret := C.pmpz_cmp(m.Ptr(), mpz.Ptr())
+	return int(ret)
 }
 
 // int pmpz_cmp_d(const unsafe_mpz op1, double op2);
-func (m Mpz) CmpDMpz(val C.double) C.int {
-	return C.pmpz_cmp_d(m.Ptr(), val)
+func (m Mpz) CmpD(val int) int {
+	ret := C.pmpz_cmp_d(m.Ptr(), C.double(val))
+	return int(ret)
 }
 
 // int pmpz_cmp_si(const unsafe_mpz op1, signed long int op2);
-func (m Mpz) CmpSiMpz(val C.long) C.int {
-	return C.pmpz_cmp_si(m.Ptr(), val)
+func (m Mpz) CmpSi(val int) int {
+	ret := C.pmpz_cmp_si(m.Ptr(), C.long(val))
+	return int(ret)
 }
 
 // int pmpz_cmp_ui(const unsafe_mpz op1, unsigned long int op2);
-func (m Mpz) CmpUiMpz(val C.ulong) C.int {
-	return C.pmpz_cmp_ui(m.Ptr(), val)
+func (m Mpz) CmpUi(val uint) int {
+	ret := C.pmpz_cmp_ui(m.Ptr(), C.ulong(val))
+	return int(ret)
 }
 
 // int pmpz_cmpabs(const unsafe_mpz op1, const unsafe_mpz op2);
-func (m Mpz) CmpAbs(mpz Mpz) C.int {
-	return C.pmpz_cmpabs(m.Ptr(), mpz.Ptr())
+func (m Mpz) CmpAbs(mpz Mpz) int {
+	ret := C.pmpz_cmpabs(m.Ptr(), mpz.Ptr())
+	return int(ret)
 }
 
 // int pmpz_cmpabs_d(const unsafe_mpz op1, double op2);
-func (m Mpz) CmpAbsDMpz(val C.double) C.int {
-	return C.pmpz_cmpabs_d(m.Ptr(), val)
+func (m Mpz) CmpAbsD(val float64) int {
+	ret := C.pmpz_cmpabs_d(m.Ptr(), C.double(val))
+	return int(ret)
 }
 
 // int pmpz_cmpabs_ui(const unsafe_mpz op1, unsigned long int op2);
-func (m Mpz) CmpAbsUiMpz(val C.ulong) C.int {
-	return C.pmpz_cmpabs_ui(m.Ptr(), val)
+func (m Mpz) CmpAbsUi(val uint) int {
+	ret := C.pmpz_cmpabs_ui(m.Ptr(), C.ulong(val))
+	return int(ret)
 }
 
 // int pmpz_sgn(const unsafe_mpz op);
-func (m Mpz) SignMpz() C.int {
-	return C.pmpz_sgn(m.Ptr())
+func (m Mpz) Sign() int {
+	ret := C.pmpz_sgn(m.Ptr())
+	return int(ret)
 }
