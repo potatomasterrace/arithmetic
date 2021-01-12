a454faad255a5b7ec23db8dec9cd26d76e02c2c4
potatomasterrace <reda.bourial@gmail.com>
2021-01-12 23:36:21 +0100

    changed return types to golang's native types

--- mpz/convert.go
+++ mpz/convert.go
@@ -10,8 +10,8 @@ import "C"
 
 // Conversion Functions
 // char *pmpz_get_str(const unsafe_mpz n, const int base);
-func (m Mpz) GetString(base C.int) string {
-	str := C.pmpz_get_str(m.Ptr(), base)
+func (m Mpz) GetString(base int) string {
+	str := C.pmpz_get_str(m.Ptr(), C.int(base))
 	defer C.free(unsafe.Pointer(str))
 	goStr := C.GoString(str)
 	return goStr
@@ -22,22 +22,26 @@ func (m Mpz) String() string {
 }
 
 // double pmpz_get_d(const unsafe_mpz op);
-func (m Mpz) GetDouble() C.double {
-	return C.pmpz_get_d(m.Ptr())
+func (m Mpz) GetDouble() float64 {
+	ret := C.pmpz_get_d(m.Ptr())
+	return float64(ret)
 }
 
 // double pmpz_get_d_2exp(signed long int *exp, const unsafe_mpz op);
-func (m Mpz) GetD2Exp() (val C.double, exp C.long) {
-	val = C.pmpz_get_d_2exp(&exp, m.Ptr())
-	return val, exp
+func (m Mpz) GetD2Exp() (v float64, e int) {
+	expC := C.long(0)
+	val := C.pmpz_get_d_2exp(&expC, m.Ptr())
+	return float64(val), int(expC)
 }
 
 // signed long int pmpz_get_si(const unsafe_mpz op);
-func (m Mpz) GetSignedInt() C.long {
-	return C.pmpz_get_si(m.Ptr())
+func (m Mpz) GetSignedInt() int {
+	ret := C.pmpz_get_si(m.Ptr())
+	return int(ret)
 }
 
 // unsigned long int pmpz_get_ui(const unsafe_mpz op);
-func (m Mpz) GetUnsignedInt() C.ulong {
-	return C.pmpz_get_ui(m.Ptr())
+func (m Mpz) GetUnsignedInt() uint {
+	ret := C.pmpz_get_ui(m.Ptr())
+	return uint(ret)
 }
